a8b7db6526e1500ea9eb46852fe2000bf0a8e71a
ravi4972 <34796993+ravi4972@users.noreply.github.com>
2025-04-19 08:51:06 +0000

    initial commit for react parcel

---
+++ React using Parcel/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+const header = React.createElement("h1",{},"React with parcel")
+
+root.render(header)
