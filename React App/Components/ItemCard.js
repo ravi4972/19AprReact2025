251804782f3e3c3ff2dfe44685c2a25f175562ff
ravi4972 <34796993+ravi4972@users.noreply.github.com>
2025-04-30 08:18:54 +0000

    items added

--- React App/Components/ItemCard.js
+++ React App/Components/ItemCard.js
@@ -2,11 +2,21 @@ export const ItemCard = (props)=>{
     const {name,tag,price,stock} = props
     return(
         <div className="itemcard">
-            <img src="https://cdn.vectorstock.com/i/2000v/27/90/new-item-sign-or-stamp-vector-32342790.avif" alt="item-logo"/>
-            <h2>{name}</h2>
-            <h3>{tag.join(",")}</h3>
-            <h3>Rs. {price}</h3>
-            <h3>{stock}</h3>
+            <div className="itemHeader">
+                <img src="https://cdn.vectorstock.com/i/2000v/27/90/new-item-sign-or-stamp-vector-32342790.avif" alt="item-logo"/>
+                <h2>{name}</h2>
+                <h3>{tag.join(",")}</h3>
+            </div>
+            
+            <div className="iteminfo">
+                <div className="left-info">
+                    <h3>Rs. {price}</h3>
+                    <h3>Stock: {stock}</h3>
+                </div>
+                <div className="right-action">
+                    <button>Add to Cart</button>
+                </div>
+            </div>
         </div>
     )
 }
